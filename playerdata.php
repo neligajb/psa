32cdae5d9a10094f5f881ecd1cb5706e13a5e0bb
Boone N <boone.neligan@gmail.com>
2016-03-06 16:22:51 -0500

    add insertion functions

--- playerdata.php
+++ playerdata.php
@@ -8,19 +8,9 @@ global $db_address;
 global $db_user;
 global $db_name;
 
-//if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')
-//  die('Bad POST request.');
-
 
 //set timezone
 date_default_timezone_set('UTC');
-
-//check for POST data
-$postData = array();
-if (isset($_POST))
-{
-  $postData = json_decode(file_get_contents('php://input'), true);
-}
 
 //connect to db
 $mysqli = new mysqli($db_address, $db_user, $db_password, $db_name);
@@ -30,6 +20,45 @@ if ($mysqli->connect_errno)
   die();
 }
 
+//check for POST data to determine function call
+$postData = array();
+if (isset($_POST))
+{
+  $postData = json_decode(file_get_contents('php://input'), true);
+  if ($postData != NULL && $postData['action'] == "addPlayer")
+  {
+    addPlayer($postData, $mysqli);
+    mysqli_close($mysqli);
+    die('Player Added');
+  }
+  else if ($postData != NULL && $postData['action'] == "addTournament")
+  {
+    addTournament($postData, $mysqli);
+    mysqli_close($mysqli);
+    die('Tournament Added');
+  }
+  else if ($postData != NULL && $postData['action'] == "addSponsorship")
+  {
+    addSponsorship($postData, $mysqli);
+    mysqli_close($mysqli);
+    die('Sponsorship Added');
+  }
+  else if ($postData != NULL && $postData['action'] == "addSponsor")
+  {
+    addSponsor($postData, $mysqli);
+    mysqli_close($mysqli);
+    die('Sponsor Added');
+  }
+  else if ($postData != NULL && $postData['action'] == "addCountry")
+  {
+    addCountry($postData, $mysqli);
+    mysqli_close($mysqli);
+    die('Country Added');
+  }
+}
+
+
+//initialize response array
 $psaData = array();
 
 //establish base selection query
@@ -41,12 +70,14 @@ $sponsor_filter = false;
 $country_filter = false;
 $tournament_filter= false;
 
+
 //check if filtering by sponsor
 if (isset($_GET['sponsor']) && ($_GET['sponsor'] != "undefined") && ($_GET['sponsor'] != "All"))
 {
   $select_statement .= " INNER JOIN psa_player_sponsor ps ON ps.pid=p.pid INNER JOIN psa_sponsors s ON s.sid=ps.sid";
   $sponsor_filter = true;
 }
+
 
 //check if we're filtering by tournament
 if (isset($_GET['tournament']) && ($_GET['tournament'] != "undefined") && ($_GET['tournament'] != "All"))
@@ -55,6 +86,7 @@ if (isset($_GET['tournament']) && ($_GET['tournament'] != "undefined") && ($_GET
   $select_statement .= " INNER JOIN psa_tournaments t ON (p.pid=t.winner OR p.pid=t.runner_up)";
   $tournament_filter = true;
 }
+
 
 //apply sponsor and/or tournament WHERE clauses
 $select_statement .= ($sponsor_filter) ? " WHERE s.name=" . "'" . $_GET['sponsor'] . "'" : '';
@@ -91,6 +123,7 @@ $out_lname = NULL;
 $out_rank = NULL;
 $out_dob = NULL;
 $out_country = NULL;
+$out_cid = NULL;
 
 if (!$stmt->bind_result($out_pid, $out_fname, $out_lname, $out_rank, $out_dob, $out_country))
   echo "Binding result failed: (" . $stmt->errno . ") " . $stmt->error;
@@ -142,12 +175,13 @@ foreach ($players as $key => $player)
 
 $psaData['players'] = $players;
 
+
 //get full country list
 $stmt = NULL;
 $countries = array("All");
 $out_country = NULL;
 
-if (!($stmt = $mysqli->prepare("SELECT name FROM psa_countries;")))
+if (!($stmt = $mysqli->prepare("SELECT name FROM psa_countries ORDER BY name ASC;")))
   echo "Prepared statement failed: (" . $mysqli->errno . ") " . $mysqli->error;
 
 if (!$stmt->execute())
@@ -164,12 +198,13 @@ while ($stmt->fetch())
 //add countries to psaData
 $psaData['countries'] = $countries;
 
+
 //get full sponsor list
 $stmt = NULL;
 $sponsors = array("All");
 $out_sponsor = NULL;
 
-if (!($stmt = $mysqli->prepare("SELECT name FROM psa_sponsors;")))
+if (!($stmt = $mysqli->prepare("SELECT name FROM psa_sponsors ORDER BY name ASC;")))
   echo "Prepared statement failed: (" . $mysqli->errno . ") " . $mysqli->error;
 
 if (!$stmt->execute())
@@ -186,12 +221,13 @@ while ($stmt->fetch())
 //add sponsors to psaData
 $psaData['sponsors'] = $sponsors;
 
+
 //get full tournament list
 $stmt = NULL;
 $tournaments = array("All");
 $out_tournament = NULL;
 
-if (!($stmt = $mysqli->prepare("SELECT DISTINCT name FROM psa_tournaments;")))
+if (!($stmt = $mysqli->prepare("SELECT DISTINCT name FROM psa_tournaments ORDER BY name ASC;")))
   echo "Prepared statement failed: (" . $mysqli->errno . ") " . $mysqli->error;
 
 if (!$stmt->execute())
@@ -209,9 +245,213 @@ while ($stmt->fetch())
 $psaData['tournaments'] = $tournaments;
 
 
-mysqli_close($mysqli);
+//get count of players in the DB
+$stmt = NULL;
+$out_count = NULL;
 
+if (!($stmt = $mysqli->prepare("SELECT COUNT(*) FROM psa_players;")))
+  echo "Prepared statement failed: (" . $mysqli->errno . ") " . $mysqli->error;
+
+if (!$stmt->execute())
+  echo "Execute statement failed: (" . $mysqli->errno . ") " . $mysqli->error;
+
+if (!$stmt->bind_result($out_count))
+  echo "Binding result failed: (" . $stmt->errno . ") " . $stmt->error;
+
+while ($stmt->fetch())
+{
+  //do nothing
+}
+
+//add player count to psaData
+$psaData['count'] = $out_count;
+
+//close connection and output JSON
+mysqli_close($mysqli);
 if (!$jsonStr = json_encode($psaData, JSON_FORCE_OBJECT))
   die('could not encode JSON');
 else
   die($jsonStr);
+
+
+
+//function definitions
+function addPlayer($data, $db_connection)
+{
+  //get country id
+  if (!($stmt = $db_connection->prepare("SELECT cid FROM psa_countries WHERE name = ?;")))
+    echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->bind_param('s', $data['country']))
+    echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->execute())
+    echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->store_result())
+    echo "Store result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  $country_id = NULL;
+
+  if (!$stmt->bind_result($country_id))
+    echo "Binding result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  while($stmt->fetch())
+  {
+    if (!($stmt2 = $db_connection->prepare("INSERT INTO psa_players (fname, lname, rank, country, dob) 
+    VALUES (?, ?, ?, ?, ?)")))
+    {
+      echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+    }
+    
+    if (!$stmt2->bind_param('ssiis', $data['fname'], $data['lname'], $data['rank'], $country_id, $data['dob']))
+          echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+    
+    if (!$stmt2->execute())
+      echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->store_result())
+      echo "Store result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+    
+    $stmt2->close();
+  }
+  return;
+}
+
+
+function addTournament ($data, $db_connection)
+{
+  if (!($stmt = $db_connection->prepare("SELECT cid FROM psa_countries WHERE name = ?;")))
+    echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->bind_param('s', $data['country']))
+    echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+  
+  if (!$stmt->execute())
+    echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->store_result())
+    echo "Binding result failed: (" . $stmt->errno . ") " . $stmt->error;
+
+  $country_id = NULL;
+
+  if (!$stmt->bind_result($country_id))
+    echo "Binding result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  while($stmt->fetch()) {
+    if (!($stmt2 = $db_connection->prepare("INSERT INTO psa_tournaments 
+      (name, winner, runner_up, country, city, prize_money, year)  
+       VALUES (?, ?, ?, ?, ?, ?, ?)"))
+    ) {
+      echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+    }
+
+    if (!$stmt2->bind_param('siiisis', $data['name'], $data['winner'], $data['runnerup'], $country_id, $data['city'],
+        $data['prize_money'], $data['year']))
+    {
+      echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+    }
+
+    if (!$stmt2->execute())
+      echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->store_result())
+      echo "Store result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    $stmt2->close();
+  }
+  return;
+}
+
+
+function addSponsorship ($data, $db_connection)
+{
+  if (!($stmt = $db_connection->prepare("SELECT sid FROM psa_sponsors WHERE name = ?;")))
+    echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->bind_param('s', $data['sponsor']))
+    echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->execute())
+    echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->store_result())
+    echo "Binding result failed: (" . $stmt->errno . ") " . $stmt->error;
+
+  $sponsor_id = NULL;
+
+  if (!$stmt->bind_result($sponsor_id))
+    echo "Binding result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  while($stmt->fetch()) {
+    if (!($stmt2 = $db_connection->prepare("INSERT INTO psa_player_sponsor (pid, sid) VALUES (?, ?)")))
+      echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->bind_param('ii', $data['player'], $sponsor_id))
+      echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->execute())
+      echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->store_result())
+      echo "Store result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    $stmt2->close();
+  }
+  return;
+}
+
+
+function addSponsor ($data, $db_connection)
+{
+  if (!($stmt = $db_connection->prepare("SELECT cid FROM psa_countries WHERE name = ?;")))
+    echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->bind_param('s', $data['country']))
+    echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->execute())
+    echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->store_result())
+    echo "Binding result failed: (" . $stmt->errno . ") " . $stmt->error;
+
+  $country_id = NULL;
+
+  if (!$stmt->bind_result($country_id))
+    echo "Binding result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  while($stmt->fetch()) {
+    if (!($stmt2 = $db_connection->prepare("INSERT INTO psa_sponsors (name, country) VALUES (?, ?)")))
+      echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->bind_param('si', $data['name'], $country_id))
+      echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->execute())
+      echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    if (!$stmt2->store_result())
+      echo "Store result failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+    $stmt2->close();
+  }
+  return;
+}
+
+
+function addCountry ($data, $db_connection)
+{
+  if (!($stmt = $db_connection->prepare("INSERT INTO psa_countries (name, population) VALUES (?, ?)")))
+    echo "Prepared statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->bind_param('si', $data['name'], $data['population']))
+    echo "Binding param failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  if (!$stmt->execute())
+    echo "Execute statement failed: (" . $db_connection->errno . ") " . $db_connection->error;
+
+  $stmt->close();
+
+  return;
+}
--- playerslist.js
+++ playerslist.js
@@ -19,15 +19,81 @@ app.controller('GetPsaData', function($scope, $http) {
             method: 'POST',
             url: 'playerdata.php',
             data: {
+                'action' : 'addPlayer',
                 'fname': $scope.new_fname,
                 'lname': $scope.new_lname,
+                'rank' : $scope.new_rank,
                 'dob': $scope.new_dob,
                 'country': $scope.new_country,
-                'sponsor': $scope.new_sponsor
             },
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).success(function () {
-            alert('Player Added');
+        }).success(function(data) {
+            alert(data.toString());
+        });
+    };
+
+    $scope.addTournament = function () {
+        $http({
+            method: 'POST',
+            url: 'playerdata.php',
+            data: {
+                'action' : 'addTournament',
+                'name': $scope.new_tournament_name,
+                'winner': $scope.new_winner.id,
+                'runnerup' : $scope.new_runnerup.id,
+                'country': $scope.new_tournament_country,
+                'city' : $scope.new_tournament_city,
+                'prize_money' : $scope.new_tournament_prize_money,
+                'year' : $scope.new_tournament_year
+            },
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            alert(data.toString());
+        });
+    };
+
+    $scope.addSponsorship = function () {
+        $http({
+            method: 'POST',
+            url: 'playerdata.php',
+            data: {
+                'action' : 'addSponsorship',
+                'player': $scope.new_ps_player.id,
+                'sponsor': $scope.new_ps_sponsor
+            },
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            alert(data.toString());
+        });
+    };
+
+    $scope.addSponsor = function () {
+        $http({
+            method: 'POST',
+            url: 'playerdata.php',
+            data: {
+                'action' : 'addSponsor',
+                'name': $scope.new_sponsor,
+                'country': $scope.new_sponsor_country
+            },
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            alert(data.toString());
+        });
+    };
+
+    $scope.addCountry = function () {
+        $http({
+            method: 'POST',
+            url: 'playerdata.php',
+            data: {
+                'action' : 'addCountry',
+                'name': $scope.new_country,
+                'population': $scope.new_population
+            },
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            alert(data.toString());
         });
     };
 });
